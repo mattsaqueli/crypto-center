b88730a3e1f1b945cc5ffde0743283020afb54e6
Matt Saqueli <msaqueli22@turing.edu>
2023-06-19 10:20:56 -0500

    feat: Add Router and display crypto details page

--- src/Components/CryptoCards/CryptoCards.js
+++ src/Components/CryptoCards/CryptoCards.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './CryptoCards.css'
+import { Link } from 'react-router-dom'
 
 const Cryptos = (props) => {
   const cryptoCards = props.displayCryptos.map(crypto => {
     return (
-      <div className='crypto-card'>
+      <Link to={`/crypto/${crypto.id}`} className='crypto-card' key={crypto.id}>
         <h3>{crypto.name}</h3>
         <h4>{crypto.symbol}</h4>
         <h5>${crypto.priceUsd}</h5>
-      </div>
+      </Link>
     )
   })
 
