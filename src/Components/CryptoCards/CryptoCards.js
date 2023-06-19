0170b18c42868878ac8a66d11bc418a6beaf8621
Matt Saqueli <msaqueli22@turing.edu>
2023-06-19 11:05:35 -0500

    feat: Update crypto displays, add additional info

--- src/Components/CryptoCards/CryptoCards.js
+++ src/Components/CryptoCards/CryptoCards.js
@@ -4,19 +4,31 @@ import { Link } from 'react-router-dom'
 
 const Cryptos = (props) => {
   const cryptoCards = props.displayCryptos.map(crypto => {
+    const formattedPrice = parseFloat(crypto.priceUsd).toFixed(2)
+    const formattedMarketCap = parseFloat(crypto.marketCapUsd).toFixed(2)
+
     return (
       <Link to={`/crypto/${crypto.id}`} className='crypto-card' key={crypto.id}>
-        <h3>{crypto.name}</h3>
+        <h4>{crypto.rank}</h4>
+        <h4>{crypto.name}</h4>
         <h4>{crypto.symbol}</h4>
-        <h5>${crypto.priceUsd}</h5>
+        <h4>${formattedPrice}</h4>
+        <h4>${formattedMarketCap}</h4>
       </Link>
     )
   })
 
   return (
-    <div className='crypto-container'>
+    <table className='crypto-container'>
+      <tr className='crypto-container-header'>
+        <th>Rank</th>
+        <th>Name</th>
+        <th>Ticker</th>
+        <th>Current Price</th>
+        <th>Market Cap</th>
+      </tr>
       {cryptoCards}
-    </div>
+    </table>
   )
 }
 
