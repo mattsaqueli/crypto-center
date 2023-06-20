d70524aba224bafc01f37aa8723013dd6c076d48
Matt Saqueli <msaqueli22@turing.edu>
2023-06-19 19:53:55 -0500

    feat: Add watchlist functionality

--- src/Components/CryptoCards/CryptoCards.js
+++ src/Components/CryptoCards/CryptoCards.js
@@ -2,10 +2,20 @@ import React from 'react'
 import './CryptoCards.css'
 import { Link } from 'react-router-dom'
 
-const Cryptos = (props) => {
-  const cryptoCards = props.displayCryptos.map(crypto => {
+const CryptoCards = ({ displayCryptos, addToWatchlist, removeFromWatchlist, isCryptoInWatchlist }) => {
+
+  const cryptoCards = displayCryptos.map((crypto) => {
     const formattedPrice = parseFloat(crypto.priceUsd).toFixed(2)
     const formattedMarketCap = parseFloat(crypto.marketCapUsd).toFixed(2)
+
+    const handleWatchlistToggle = () => {
+      if (isCryptoInWatchlist(crypto.id)) {
+        removeFromWatchlist(crypto.id)
+      } else {
+        addToWatchlist(crypto.id)
+      }
+    }
+
 
     return (
       <Link to={`/crypto/${crypto.id}`} className='crypto-card' key={crypto.id}>
@@ -14,6 +24,9 @@ const Cryptos = (props) => {
         <h4>{crypto.symbol}</h4>
         <h4>${formattedPrice}</h4>
         <h4>${formattedMarketCap}</h4>
+      <button onClick={handleWatchlistToggle}>
+        {isCryptoInWatchlist(crypto.id) ? 'Remove from Watchlist' : 'Add to Watchlist'}
+      </button>
       </Link>
     )
   })
@@ -26,10 +39,11 @@ const Cryptos = (props) => {
         <th>Ticker</th>
         <th>Current Price</th>
         <th>Market Cap</th>
+        <th>Watchlist</th>
       </tr>
       {cryptoCards}
     </table>
   )
 }
 
-export default Cryptos
+export default CryptoCards
